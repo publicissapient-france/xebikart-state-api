c6fddeeaaf18af1d3b75bebdc64c737a0d02ba1b
Jean-Pascal THIERY <jpthiery@xebia.fr>
2019-09-06 23:12:10 +0200

    :loud_sound: Log healthcheck (#30)

--- src/main/java/fr/xebia/xebicon/xebikart/api/infra/http/endpoint/RootEndpoint.java
+++ src/main/java/fr/xebia/xebicon/xebikart/api/infra/http/endpoint/RootEndpoint.java
@@ -1,16 +1,23 @@
 package fr.xebia.xebicon.xebikart.api.infra.http.endpoint;
 
+import org.slf4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class RootEndpoint extends HttpServlet {
+
+    private static final Logger LOGGER = getLogger(RootEndpoint.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+        LOGGER.trace("healthcheck called");
+        resp.setStatus(HttpServletResponse.SC_OK);
     }
 
 }
