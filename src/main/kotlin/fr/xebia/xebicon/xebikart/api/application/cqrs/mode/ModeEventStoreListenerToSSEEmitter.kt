764035005effdec634362e1d66ddf0bb330e8f00
Benjamin Lacroix <blacroix@xebia.fr>
2019-11-14 22:14:55 +0100

    feat(mode): filter mode by instance

--- src/main/kotlin/fr/xebia/xebicon/xebikart/api/application/cqrs/mode/ModeEventStoreListenerToSSEEmitter.kt
+++ src/main/kotlin/fr/xebia/xebicon/xebikart/api/application/cqrs/mode/ModeEventStoreListenerToSSEEmitter.kt
@@ -4,22 +4,29 @@ import com.google.gson.JsonParseException
 import fr.xebia.xebicon.xebikart.api.application.bus.EventEmitter
 import fr.xebia.xebicon.xebikart.api.application.cqrs.Event
 import fr.xebia.xebicon.xebikart.api.application.cqrs.EventStoreListener
+import fr.xebia.xebicon.xebikart.api.application.cqrs.ModeEvent
 import fr.xebia.xebicon.xebikart.api.application.cqrs.ModeSet
 import fr.xebia.xebicon.xebikart.api.infra.GsonProvider
+import fr.xebia.xebicon.xebikart.api.infra.mqtt.MqttConsumerContainer
+import org.jetbrains.annotations.NotNull
 import org.slf4j.LoggerFactory
 
-class ModeEventStoreListenerToSSEEmitter(private val eventEmitter: EventEmitter) : EventStoreListener {
+class ModeEventStoreListenerToSSEEmitter(
+        private val eventEmitter: @NotNull EventEmitter,
+        private val eventMqttConsumerContainer: MqttConsumerContainer) : EventStoreListener {
     private val logger = LoggerFactory.getLogger(ModeEventStoreListenerToSSEEmitter::class.java)
 
     override fun <E : Event> eventsAppenned(events: List<E>) {
         val provideGson = GsonProvider.provideGson()
-        events.forEach {
-            try {
-                eventEmitter.send(ModeSet::class.java.simpleName, provideGson.toJson(it))
-            } catch (e: JsonParseException) {
-                logger.warn("Cannot send event ${it.identifier()}")
-            }
-        }
+        events.filterIsInstance<ModeEvent>()
+                .forEach {
+                    try {
+                        eventEmitter.send(ModeSet::class.java.simpleName, provideGson.toJson(it))
+                        eventMqttConsumerContainer.publish()
+                    } catch (e: JsonParseException) {
+                        logger.warn("Cannot send event ${it.identifier()}")
+                    }
+                }
 
     }
 }
