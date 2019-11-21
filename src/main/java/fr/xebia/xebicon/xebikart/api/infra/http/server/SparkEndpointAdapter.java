baf25045a3a5eb6d8d9f0de7cb595f9ea363db5b
bartr0n <jose.martinsanchez@outlook.fr>
2019-11-21 15:38:15 +0100

    Add custom Prometheus metrics for all Spark endpoints (#51)
    * Add custom Prometheus metrics for all Spark endpoints
    
    * Use constant for timer attribute

--- src/main/java/fr/xebia/xebicon/xebikart/api/infra/http/server/SparkEndpointAdapter.java
+++ src/main/java/fr/xebia/xebicon/xebikart/api/infra/http/server/SparkEndpointAdapter.java
@@ -1,6 +1,8 @@
 package fr.xebia.xebicon.xebikart.api.infra.http.server;
 
 import fr.xebia.xebicon.xebikart.api.infra.http.endpoint.SparkEndpoint;
+import io.prometheus.client.Counter;
+import io.prometheus.client.Summary;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import spark.Spark;
@@ -9,12 +11,26 @@ import spark.servlet.SparkApplication;
 import java.util.Set;
 
 import static java.util.Objects.requireNonNull;
-import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.slf4j.LoggerFactory.getLogger;
+import static spark.Spark.*;
 
 public class SparkEndpointAdapter implements SparkApplication {
 
     private static final Logger LOGGER = getLogger(SparkEndpointAdapter.class);
+
+    // Custom Prometheus metrics
+    private final static String TIMER_ATTRIBUTE = "timer";
+
+    private static final Counter httpRequestByEndpoint = Counter.build("api_http_request_count", "HTTP requests by endpoint")
+            .labelNames("method", "endpoint", "status")
+            .register();
+
+    private static final Summary requestLatencyByEndpoint = Summary.build()
+            .name("api_request_latency")
+            .help("Request latency")
+            .labelNames("method", "endpoint")
+            .create()
+            .register();
 
     private final String pathPrefix;
     private final Set<SparkEndpoint> sparkEndpoints;
@@ -27,12 +43,25 @@ public class SparkEndpointAdapter implements SparkApplication {
 
     @Override
     public void init() {
-        Spark.before("*", (request, response) -> response.header("Content-Type", "application/json"));
+        before("*", (request, response) -> {
+            Summary.Timer requestTimer = requestLatencyByEndpoint.labels(request.requestMethod(), request.pathInfo())
+                    .startTimer();
+            request.attribute(TIMER_ATTRIBUTE, requestTimer); // Pass timer into the filter chain
+
+            response.header("Content-Type", "application/json");
+        });
+        afterAfter("/*", ((request, response) -> {
+            Summary.Timer timer = request.attribute(TIMER_ATTRIBUTE);
+            timer.observeDuration();
+
+            httpRequestByEndpoint.labels(request.requestMethod(), request.pathInfo(), String.valueOf(response.status()))
+                    .inc();
+        }));
         if (StringUtils.isBlank(pathPrefix)) {
             sparkEndpoints.forEach(SparkEndpoint::configure);
         } else {
             LOGGER.info("Prefix Spark endpoint by {}.", pathPrefix);
-            Spark.path(pathPrefix, () -> sparkEndpoints.forEach(SparkEndpoint::configure));
+            path(pathPrefix, () -> sparkEndpoints.forEach(SparkEndpoint::configure));
         }
     }
 
